49b2a8801e0c8f59679256a078cf0712bb95a2fb
Johannah Sims <jjrsims@gmail.com>
2018-01-10 16:16:38 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
 Def display_rainbow(prism)
   puts "R: #{prism[0]}, O: #{prism[1]}, Y: #{prism[2]}, G: #{prism[3]}, B: #{prism[4]}, I: #{prism[5]}, V: #{prism[6]}"
